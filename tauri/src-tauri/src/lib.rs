f69ae323cf3cbfa5a5f30be4b7e51a8c4d3d0225
raheem-tiamiyu <raheem.tiamiyu@encana.com>
2024-11-05 16:03:25 -0700

    add update logic

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ async fn python_search_exe(app: AppHandle, keys: String, columns: String, paths:
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
--- tauri/src/App.jsx
+++ tauri/src/App.jsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import { check } from "@tauri-apps/plugin-updater";
-import { ask, message } from "@tauri-apps/plugin-dialog";
+import { relaunch } from "@tauri-apps/plugin-process";
 import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import "react-toastify/dist/ReactToastify.css";
@@ -15,40 +15,41 @@ function App() {
     // You can update the UI with the received JSON data here
   });
 
-  async function checkForAppUpdates(onUserClick) {
-    const update = await check();
-    if (update === null) {
-      await message("Failed to check for updates.\nPlease try again later.", {
-        title: "Error",
-        kind: "error",
-        okLabel: "OK",
-      });
-      return;
-    } else if (update?.available) {
-      const yes = await ask(
-        `Update to ${update.version} is available!\n\nRelease notes: ${update.body}`,
-        {
-          title: "Update Available",
-          kind: "info",
-          okLabel: "Update",
-          cancelLabel: "Cancel",
-        }
+  async function checkUpdate(params) {
+    const update = await check({
+      target: "windows-x86_64",
+    });
+    console.log(update);
+    if (update) {
+      console.log(
+        `found update ${update.version} from ${update.date} with notes ${update.body}`
       );
-      if (yes) {
-        await update.downloadAndInstall();
-        // Restart the app after the update is installed by calling the Tauri command that handles restart for your app
-        // It is good practice to shut down any background processes gracefully before restarting
-        // As an alternative, you could ask the user to restart the app manually
-        await invoke("graceful_restart");
-      }
-    } else if (onUserClick) {
-      await message("You are on the latest version. Stay awesome!", {
-        title: "No Update Available",
-        kind: "info",
-        okLabel: "OK",
+      let downloaded = 0;
+      let contentLength = 0;
+      // alternatively we could also call update.download() and update.install() separately
+      await update.downloadAndInstall((event) => {
+        switch (event.event) {
+          case "Started":
+            contentLength = event.data.contentLength;
+            console.log(
+              `started downloading ${event.data.contentLength} bytes`
+            );
+            break;
+          case "Progress":
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+          case "Finished":
+            console.log("download finished");
+            break;
+        }
       });
+
+      console.log("update installed");
+      await relaunch();
     }
   }
+  checkUpdate();
 
   return (
     <main className="container">
